25ff6893b4fe1fe43971ebc66664c89f6a4d9b89
nourhene <benothmennourhen8@gmail.com>
2025-11-07 21:27:45 +0100

    🗄️ Implement userController functions for CRUD operations

---
+++ src/controllers/userController.js
@@ -0,0 +1,26 @@
+const pool = require('../config/database');
+
+// Create a new user
+exports.createUser = async (req, res) => {
+  // Logic to create a user in database
+};
+
+// Get all users
+exports.getAllUsers = async (req, res) => {
+  // Logic to fetch all users
+};
+
+// Get a single user by ID
+exports.getUserById = async (req, res) => {
+  // Logic to fetch one user by ID
+};
+
+// Update a user by ID
+exports.updateUser = async (req, res) => {
+  // Logic to update user
+};
+
+// Delete a user by ID
+exports.deleteUser = async (req, res) => {
+  // Logic to delete user
+};
